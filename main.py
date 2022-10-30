1a9b7f20886afae465641a23d29ce59340991e5c
BEASTUSERBOT <95837974+BEASTUSERBOT@users.noreply.github.com>
2022-10-30 09:36:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ app = Flask(__name__)
 
 #uvicorn main:app --host 192.168.18.244
 
-@app.route("/VjFod1QxUXlUak5RVkRBOQ==/{password}")
-def login():
+@app.route("/VjFod1QxUXlUak5RVkRBOQ==/<p>")
+def login(p):
     return render_template('k.html')
 
 @app.route("/L1ZqRm9kMVF4VVhsVWFrNVJWa1JCT1E9PS97cGFzc3dvcmR9/")
 def closeDoor():
     return render_template('look.html')
 
+#pip freeze > to-uninstall.txt
+#pip uninstall -r to-uninstall.txt
 
 if __name__ == '__main__':
     app.run()
